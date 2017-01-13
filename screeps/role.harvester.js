f7eede6f4513098a699f21a4391828d18445ac4b
ahale <andy@tannhauser-gate.org>
2017-01-13 17:27:42 +0000

    initial commit

--- screeps/role.harvester.js
+++ screeps/role.harvester.js
@@ -2,12 +2,22 @@
 module.exports = {
     run: function(creep) {
         console.log('todo: '+creep.memory.role+' specific stuff');
+        var room_mode = creep.room.memory.mode;
         var source_mode = creep.room.memory.sources[creep.memory.src].mode;
         // var res = eval(creep.memory.role+'.run')(creep);
-        eval('this.'+source_mode)(creep);
+        if(room_mode == "setup") {
+            this.setup(creep);
+        }
+        else {
+            eval('this.'+source_mode)(creep);
+        }
     },
 
     setup: function(creep) {
+        console.log('todo: harvester in initial setup mode');
+    },
+
+    harvest: function(creep) {
         console.log('todo: harvester in initial setup mode');
     },
 
