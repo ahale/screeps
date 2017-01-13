40a17523f9515c7283122904e8b0a25f5ebf51a5
ahale <andy@tannhauser-gate.org>
2017-01-13 20:22:02 +0000

    initial commit

--- screeps/role.base.js
+++ screeps/role.base.js
@@ -15,10 +15,12 @@ module.exports = {
         if(creep.memory.work == undefined) { creep.memory.work = {"work": 0, "wait": 0, "move": 0, "harvest": 0}; }
 
         // new RoomPosition(Game.spawns.Spawn1.pos.x, (Game.spawns.Spawn1.pos.y - 3), name)
-        if((creep.pos.roomname == creep.memory._move.dest.room) &&
-           ((creep.pos.x != creep.memory._move.dest.x) || (creep.pos.y != creep.memory._move.dest.y))) {
-               creep.memory.work.move += 1;
-           }
+        if(creep.memory._move) {
+            if((creep.pos.roomname == creep.memory._move.dest.room) &&
+               ((creep.pos.x != creep.memory._move.dest.x) || (creep.pos.y != creep.memory._move.dest.y))) {
+                   creep.memory.work.move += 1;
+            }
+        }
 
         if(creep.memory.role == "miner") { miner.run(creep); };
         if(creep.memory.role == "builder") { builder.run(creep); };
