7ffc845627a6bff55517828fdfc30ea77048438c
ahale <andy@tannhauser-gate.org>
2017-01-13 22:14:54 +0000

    initial commit

--- screeps/role.base.js
+++ screeps/role.base.js
@@ -6,22 +6,24 @@ var transporter = require('role.transporter');
 
 module.exports = {
     run: function(creep) {
-        // console.log('todo: write generic creep stuff');
+        if(creep.memory.init == undefined) { this.memory_init(creep); }
         if(creep.ticksToLive == 1) {
             console.log(creep.memory.role+" "+creep.name+" about to die: ");
             console.log(creep.memory.role+" "+creep.name+' stats: worked: '+creep.memory.work.work+', moved: '+creep.memory.work.move+', harvest: '+creep.memory.work.harvest+', wait: '+creep.memory.work.wait);
             return;
         }
 
-        if(creep.memory.src == undefined) { creep.memory.src = 0; }
-        if(creep.memory.room == undefined) { creep.memory.room = Memory.rooms.homeroom; }
-        if(creep.memory.work == undefined) { creep.memory.work = {"work": 0, "wait": 0, "move": 0, "harvest": 0}; }
-
         if(creep.memory.role == "miner") { miner.run(creep); };
         if(creep.memory.role == "builder") { builder.run(creep); };
         if(creep.memory.role == "upgrader") { upgrader.run(creep); };
         if(creep.memory.role == "harvester") { harvester.run(creep); };
         if(creep.memory.role == "transporter") { transporter.run(creep); };
+    },
+
+    memory_init: function(creep) {
+        if(creep.memory.src == undefined) { creep.memory.src = 0; }
+        if(creep.memory.room == undefined) { creep.memory.room = Memory.rooms.homeroom; }
+        if(creep.memory.work == undefined) { creep.memory.work = {"work": 0, "wait": 0, "move": 0, "harvest": 0}; }
 
     }
 }
