0bcd061535f18c3c6bace74c1909087140834072
ahale <andy@tannhauser-gate.org>
2017-01-13 20:31:45 +0000

    initial commit

--- screeps/module.factory.js
+++ screeps/module.factory.js
@@ -8,9 +8,18 @@ module.exports = {
 
     run: function(name) {
         // console.log('todo: write factory run loop');
+        this.clear_expired_creeps();
     },
 
     build: function(meta) {
         console.log('todo: write factory build function');
     },
+
+    clear_expired_creeps() {
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                delete Memory.creeps[name];
+            }
+        }
+    }
 }
