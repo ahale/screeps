163530a6a873d214771852454ee183aca14c6ba1
ahale <andy@tannhauser-gate.org>
2017-01-13 19:55:32 +0000

    initial commit

--- screeps/role.base.js
+++ screeps/role.base.js
@@ -11,16 +11,14 @@ module.exports = {
             console.log(creep.memory.role+" "+creep.name+" about to die");
             return;
         }
-        if(creep.memory.src == undefined) {
-            creep.memory.src = 0;
-        }
-        if(creep.memory.role == "harvester") { harvester.run(creep); };
+        if(creep.memory.src == undefined) { creep.memory.src = 0; }
+        if(creep.memory.work == undefined) { creep.memory.work = {"work": 0, "wait": 0, "move": 0, "harvest": 0}; }
 
-        // var res = harvester.run(creep);
-        // var cmd = ;
-        // var func = new Function('return harvester;');
-        // x = new func()
-        // console.log(x);
-        // x.run(creep);
+        if(creep.memory.role == "miner") { miner.run(creep); };
+        if(creep.memory.role == "builder") { builder.run(creep); };
+        if(creep.memory.role == "upgrader") { upgrader.run(creep); };
+        if(creep.memory.role == "harvester") { harvester.run(creep); };
+        if(creep.memory.role == "transporter") { transporter.run(creep); };
+
     }
 }
--- screeps/role.harvester.js
+++ screeps/role.harvester.js
@@ -5,22 +5,44 @@ module.exports = {
         console.log('todo: '+creep.memory.role+' specific stuff');
         var source_mode = creep.room.memory.sources[creep.memory.src].mode;
         // var res = eval(creep.memory.role+'.run')(creep);
-        if(creep.room.memory.mode == "setup") { console.log('creep: '+creep.name); this.setup(creep); };
         if(creep.room.memory.mode == "harvest") { console.log('creep: '+creep.name); this.harvest(creep); };
         if(creep.room.memory.mode == "miner") { console.log('creep: '+creep.name); this.miner(creep); };
+        if(creep.room.memory.mode == "container") { console.log('creep: '+creep.name); this.container(creep); };
         if(creep.room.memory.mode == "delivery") { console.log('creep: '+creep.name); this.delivery(creep); };
     },
 
-    setup: function(creep) {
-        console.log('todo: harvester in initial setup mode');
-    },
-
     harvest: function(creep) {
-        console.log('todo: harvester in initial setup mode');
+        console.log('todo: harvester in harvest mode');
+        if(creep.carry.energy < creep.carryCapacity) {
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+            if(targets.length == 0) {
+                creep.moveTo(Game.spawns.Spawn1.pos);
+            }
+        }
     },
 
     miner: function(creep) {
         console.log('todo: harvester in miner mode');
+    },
+
+    container: function(creep) {
+        console.log('todo: harvester in container mode');
     },
 
     delivery: function(creep) {
