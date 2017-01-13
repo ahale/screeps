1215d1fea78f6a9a3604f191b346de990de8000c
ahale <andy@tannhauser-gate.org>
2017-01-13 20:42:23 +0000

    initial commit

--- screeps/role.upgrader.js
+++ screeps/role.upgrader.js
@@ -16,6 +16,36 @@ module.exports = {
     },
 
     harvest: function(creep) {
+        if(creep.room.controller) {
+            if(creep.memory.upgrading && creep.carry.energy == 0) {
+                creep.memory.upgrading = false;
+            }
+            if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+                creep.memory.upgrading = true;
+            }
+
+            if(creep.memory.upgrading) {
+                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                    creep.memory.work.move += 1;
+                }
+                else {
+                    creep.memory.work.work += 1;
+                }
+            }
+            else {
+                var src = Game.getObjectById(creep.room.memory.sources[creep.memory.src].id);
+                if(creep.harvest(src) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(src);
+                    creep.memory.work.move += 1;
+                } else {
+                    creep.memory.work.harvest += 1;
+                }
+            }
+        else {
+            creep.moveTo(Game.spawns.Spawn1.pos);
+            creep.memory.work.wait += 1;
+        }
     },
 
     miner: function(creep) {
