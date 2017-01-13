b3cf10058f3efa03b3df55580a46783faa888563
ahale <andy@tannhauser-gate.org>
2017-01-13 17:15:13 +0000

    initial commit

--- screeps/role.base.js
+++ screeps/role.base.js
@@ -1,8 +1,8 @@
-// var miner = require('role.miner');
-// var builder = require('role.builder');
-// var upgrader = require('role.upgrader');
-// var harvester = require('role.harvester');
-// var transporter = require('role.transporter');
+var miner = require('role.miner');
+var builder = require('role.builder');
+var upgrader = require('role.upgrader');
+var harvester = require('role.harvester');
+var transporter = require('role.transporter');
 
 module.exports = {
     run: function(creep) {
