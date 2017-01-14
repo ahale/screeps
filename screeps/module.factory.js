399d873991114f07ae689bafb91b021c85830f99
ahale <andy@tannhauser-gate.org>
2017-01-14 13:04:07 +0000

    initial commit

--- screeps/module.factory.js
+++ screeps/module.factory.js
@@ -3,13 +3,29 @@ var utils = require('utils.utils');
 
 module.exports = {
     run: function(name) {
-        var size = Game.rooms[name].energyCapacityAvailable;
-        console.log('room '+name+' has energyCapacityAvailable = '+size);
-        // console.log('todo: write factory run loop');
+        // var size = Game.rooms[name].energyCapacityAvailable;
+        // console.log('room '+name+' has energyCapacityAvailable = '+size);
+        if(Game.rooms[name].memory.queues.priorityspawnqueue.length > 0) {
+            var build_data = Game.rooms[name].memory.queues.priorityspawnqueue[0];
+            var res = this.build(build_data);
+            if(_.isString(res)) {
+                console.log('building '+res+' ('+data.flavour+') (Priority)');
+                Game.rooms[name].memory.queues.priorityspawnqueue.shift();
+            }
+        }
+        if(!Game.rooms[name].memory.queues.priorityspawnqueue.length && Game.rooms[name].memory.queues.spawnqueue.length > 0) {
+            var build_data = Game.rooms[name].memory.queues.spawnqueue[0];
+            var res = this.build(build_data);
+            if(_.isString(res)) {
+                console.log('building '+res+' ('+data.flavour+')');
+                Game.rooms[name].memory.queues.spawnqueue.shift();
+            }
+        }
+
 
     },
 
-    build: function(meta) {
+    build: function(build_data) {
         console.log('todo: write factory build function');
     },
 
