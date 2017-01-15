51240f7da506f285e41bbc0469489d502bb36dde
ahale <andy@tannhauser-gate.org>
2017-01-15 14:21:45 +0000

    initial commit

--- screeps/role.upgrader.js
+++ screeps/role.upgrader.js
@@ -3,6 +3,10 @@
 module.exports = {
     run: function(creep) {
         if(!creep.memory.init) { this.memory_init(creep); }
+        if(creep.memory.static) {
+            this.harvest(creep);
+            return;
+        }
         if(creep.room.memory.mode == "harvest") { this.harvest(creep); };
         if(creep.room.memory.mode == "miner") { this.miner(creep); };
         if(creep.room.memory.mode == "container") { this.container(creep); };
