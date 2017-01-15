519d820caf8e4bc08321958939a085aff21b89e6
ahale <andy@tannhauser-gate.org>
2017-01-15 13:47:56 +0000

    initial commit

--- screeps/module.manager.js
+++ screeps/module.manager.js
@@ -31,7 +31,6 @@ module.exports = {
         }
         creeputil.check_no_creeps(name);
         factory.run(name);
-        this.stupid_stuff_to_get_creeps()
     },
 
     run_creeps: function() {
@@ -41,24 +40,4 @@ module.exports = {
         }
     },
 
-    stupid_stuff_to_get_creeps: function() {
-        var structure_builders = _.filter(Game.creeps, (creep) => (creep.memory.flavour == 'builder' && creep.memory.build_type == 'structure'));
-        var road_builders = _.filter(Game.creeps, (creep) => (creep.memory.flavour == 'builder' && creep.memory.build_type == 'road'));
-        if(!structure_builders.length) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {"flavour": "builder", "build_type": 'structure'});
-            if(_.isString(newName)) {
-                console.log('building structure builder '+newName)
-                Game.spawns['Spawn1'].memory.building = "upgrader";
-            }
-            return;
-        }
-        if(!road_builders.length && structure_builders.length < 2) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {"flavour": "builder", "build_type": 'road'});
-            if(_.isString(newName)) {
-                console.log('building road builder '+newName)
-                Game.spawns['Spawn1'].memory.building = "upgrader";
-            }
-            return;
-        }
-    },
 }
