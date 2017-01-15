a5c66c4863fcd8be0577182d8c1c593feae1f9d9
ahale <andy@tannhauser-gate.org>
2017-01-15 09:39:42 +0000

    initial commit

---
+++ screeps/role.repairer.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+    run: function(creep) {
+        if(!creep.memory.init) { this.memory_init(creep); }
+        if(creep.room.memory.mode == "harvest") { this.harvest(creep); };
+        if(creep.room.memory.mode == "miner") { this.miner(creep); };
+        if(creep.room.memory.mode == "container") { this.container(creep); };
+        if(creep.room.memory.mode == "delivery") { this.delivery(creep); };
+    },
+
+    harvest: function(creep) {
+    },
+
+    miner: function(creep) {
+    },
+
+    container: function(creep) {
+    },
+
+    delivery: function(creep) {
+    },
+
+    memory_init: function(creep) {
+        creep.memory.init = true;
+    }
+}
